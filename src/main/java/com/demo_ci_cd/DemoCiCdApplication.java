99cf2b2e620f349b30cdd103ef02bd60234cafa4
mdsaj <mdsajjadhossain614@gmail.com>
2025-04-07 02:00:41 +0600

    Add "hello word" for testing purpose

--- src/main/java/com/demo_ci_cd/DemoCiCdApplication.java
+++ src/main/java/com/demo_ci_cd/DemoCiCdApplication.java
@@ -2,12 +2,20 @@ package com.demo_ci_cd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoCiCdApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoCiCdApplication.class, args);
+    }
+
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
     }
 
 }
